93ce58d3fa3c9bce6a02d67c527386e7724e5ae0
jose-junior <joseademarti@gamil.com>
2022-10-07 16:18:19 -0300

    Finalizado o modulo 2 do curso

--- typescript-curso-1-arquivos-iniciais/app/app.ts
+++ typescript-curso-1-arquivos-iniciais/app/app.ts
@@ -1,3 +1,7 @@
 import { Negociacao } from './models/Negociacao.js';
+
 const negociacao = new Negociacao(new Date(), 100, 10);
 console.log("Volume = ", negociacao.volume);
+
+
+
--- typescript-curso-1-arquivos-iniciais/dist/js/models/Negociacao.js
+++ typescript-curso-1-arquivos-iniciais/dist/js/models/Negociacao.js
@@ -1,27 +1,19 @@
 export class Negociacao {
-    #data;
-    #valor;
-    #quantidade;
-
-    constructor(data,valor,quantidade){
-        this.#data = data;
-        this.valor =valor;
-        this.quantidade = quantidade;
+    constructor(data, valor, quantidade) {
+        this._data = data;
+        this._valor = valor;
+        this._quantidade = quantidade;
     }
-
-    get data(){
-        return this.#data;
+    get data() {
+        return this._data;
     }
-
-    get valor(){
-        return this.#valor;
+    get valor() {
+        return this._valor;
     }
-
-    get quantidade(){
-        return this.#quantidade;
+    get quantidade() {
+        return this._quantidade;
     }
-
-    get volume(){
-       return  this.#quantidade * this.#valor;
+    get volume() {
+        return this._quantidade * this._valor;
     }
 }
